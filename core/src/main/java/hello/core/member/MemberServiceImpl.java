20ce42dbaa3d28ae03b0e1b4dafe333d3aa2f806
dong <idh1007@naver.com>
2022-07-11 23:51:23 +0900

    spring 공부

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,7 +1,8 @@
 package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
-
+    //인터페이스를 의존하지만 실제 할당하는 부분에서 구현체를 의존하기 때문에 MemberServiceImpl는 추상화에도 의존하고 구체화에도 의존하는 문제점이 있음
+    //DIP 위반 - 변경할 때 문제가 발생한다
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     @Override
