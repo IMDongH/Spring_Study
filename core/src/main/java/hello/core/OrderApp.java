228cb1dd463639e8641bdc1c0d4190b427b3ce23
dong <idh1007@naver.com>
2022-07-12 15:55:24 +0900

    spring 공부 AppConfig

--- core/src/main/java/hello/core/OrderApp.java
+++ core/src/main/java/hello/core/OrderApp.java
@@ -1,15 +1,24 @@
-package hello.core.order;
+package hello.core;
 
+import hello.core.AppConfig;
 import hello.core.MemberApp;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import hello.core.order.Order;
+import hello.core.order.OrderService;
 
 public class OrderApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
+
+        AppConfig appConfig = new AppConfig();
+
+        MemberService memberService;
+        OrderService orderService;
+
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
 
         Long memberId = 1L;
         Member member = new Member(memberId,"memberA", Grade.VIP);
--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -3,8 +3,10 @@ package hello.core.member;
 public class MemberServiceImpl implements MemberService{
     //인터페이스를 의존하지만 실제 할당하는 부분에서 구현체를 의존하기 때문에 MemberServiceImpl는 추상화에도 의존하고 구체화에도 의존하는 문제점이 있음
     //DIP 위반 - 변경할 때 문제가 발생한다
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-
+    private final MemberRepository memberRepository;
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
     @Override
     public void join(Member member) {
         memberRepository.save(member);
