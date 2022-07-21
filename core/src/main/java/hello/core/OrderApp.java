1f8ee5dfd459e0ff7e2282a5f267c836c13744bd
임동혁 <>
2022-07-21 11:30:05 +0900

    Bean

--- core/src/main/java/hello/core/OrderApp.java
+++ core/src/main/java/hello/core/OrderApp.java
@@ -8,17 +8,23 @@ import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
 
-        AppConfig appConfig = new AppConfig();
-
-        MemberService memberService;
-        OrderService orderService;
-
-        memberService = appConfig.memberService();
-        orderService = appConfig.orderService();
+//        AppConfig appConfig = new AppConfig();
+//
+//        MemberService memberService;
+//        OrderService orderService;
+//
+//        memberService = appConfig.memberService();
+//        orderService = appConfig.orderService();
+//
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService",MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService",OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId,"memberA", Grade.VIP);
